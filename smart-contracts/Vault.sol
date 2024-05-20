986fa1fb3ccc0e78eb8fb913d41541b6f25c0e48
MafiaWard <30767844+MafiaWard@users.noreply.github.com>
2024-05-20 19:59:57 +0700

    feat: wire up message broker publisher in Vault.sol

--- smart-contracts/Vault.sol
+++ smart-contracts/Vault.sol
@@ -37,3 +37,4 @@ contract EnterpriseYieldVault is ReentrancyGuard, Ownable {
 // Optimized logic batch 4608
 // Optimized logic batch 3705
 // Optimized logic batch 6645
+// Optimized logic batch 9605
